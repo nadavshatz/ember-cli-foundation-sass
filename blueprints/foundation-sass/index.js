48d444d7d91250bbeddfcf00e03fe032403e37bf
Globegitter <markus.padourek@gmail.com>
2014-09-13 01:01:01 +0100

    More testing.

--- blueprints/foundation-sass/index.js
+++ blueprints/foundation-sass/index.js
@@ -2,14 +2,18 @@ var fs          = require('fs');
 var path        = require('path');
 
 module.exports = {
-  beforeInstall: function(options) {
-    console.log(options);
-    return this.addBowerPackageToProject('foundation', 'zurb/bower-foundation#5.4.2');
+  normalizeEntityName: function() {
   },
+
+  //beforeInstall: function(options) {
+  //  console.log(options);
+  //  return this.addBowerPackageToProject('foundation', 'zurb/bower-foundation#5.4.2');
+  //},
   afterInstall: function(options) {
     var appPath = path.join(process.cwd(), 'app', 'styles', 'app.css');
     console.log(options);
     console.log(appPath);
+    return true;
   }
 };
 
